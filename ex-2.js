5abb28f11acfec4bd53efb227078784879495534
Wongsathon Chuijit <wongsathon@Wongsathons-MacBook-Air.local>
2024-05-15 12:36:57 +0700

    feat:exercise done

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,13 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false;
-null == undefined;
-10 == "10";
-[10] == 10;
-[1,2] == "1,2";
+0 == false; //true
+null == undefined; //true
+10 == "10"; //true
+[10] == 10; //true
+[1, 2] == "1,2"; //true
+
+console.log(0 == false);
+console.log(null == undefined);
+console.log(10 == "10");
+console.log([10] == 10);
+console.log([1, 2] == "1,2");
